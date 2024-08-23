cd1e24a2e7b67c1c020aa2c752115c501e9e614b
jamanit <rikidavidtra.2310@gmail.com>
2024-08-23 13:37:38 +0700

    Perbarui index_product.blade.php

--- resources/views/product/index_product.blade.php
+++ resources/views/product/index_product.blade.php
@@ -21,6 +21,14 @@
                 <a href="{{ route('products.create') }}" class="btn btn-primary">Add</a>
             </div>
             <div class="card-body">
+
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        {{ session('success') }}
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                @endif
+
                 <table class="table table-hover">
                     <thead>
                         <tr>
